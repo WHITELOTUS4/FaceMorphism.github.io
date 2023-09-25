c3180dce6b5654566a61fca659b64a4420d6041e
WHITELOTUS <126754757+WHITELOTUS4@users.noreply.github.com>
2023-09-25 21:29:11 +0530

    Version 1.1

--- Components/database.js
+++ Components/database.js
@@ -8,6 +8,18 @@ const data = [
         code: "23000121018"
     },
     {
+        name: "Saptrashi Pramanik",
+        code: "23000121016"
+    },
+    {
+        name: "Rajanya Chakraborty",
+        code: "23000121002"
+    },
+    {
+        name: "Arghya Kamal ghosh",
+        code: "Student"
+    },
+    {
         name: "Anubhab Pal",
         code: "Teacher"
     },
@@ -46,6 +58,54 @@ const data = [
     {
         name: "Rajib pal",
         code: "Teacher"
+    },
+    {
+        name: "Kumaresh Manna",
+        code: "Teacher"
+    },
+    {
+        name: "Sunny Ghosh",
+        code: "Teacher"
+    },
+    {
+        name: "Albert Einstein",
+        code: "E=mc^2"
+    },
+    {
+        name: "Nikola Tesla",
+        code: "AC electricity"
+    },
+    {
+        name: "Bill Gates",
+        code: "CEO of Microsoft"
+    },
+    {
+        name: "Elon Musk",
+        code: "CEO of SpaceX"
+    },
+    {
+        name: "Mark Zuckerberg",
+        code: "CEO of Meta"
+    },
+    {
+        name: "Ratan Tata",
+        code: "Head of Tata Com"
+    },
+    {
+        name: "Steve Jobs",
+        code: "Founder of Apple"
+    },
+    {
+        name: "Sharukh Khan",
+        code: "Social Person"
+    },
+    {
+        name: "Jagannath Ghosh",
+        code: "Student"
+    },
+    {
+        name: "subhajit Biswash",
+        code: "Student"
     },
     {
         name: "Unknown",
